a4713c447bf8ad77d98ac7067cd94602b4005b18
LocDev21 <layeoumarconde2005@gmail.com>
2025-03-15 00:52:21 +0000

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,9 @@
-# Écrivez votre code ici !
+fruits={"Pomme":"rouge","banane":"jaune","orange":"orange"}
+print(fruits)
+fruits["Kiwi"]="vert"
+print(fruits)
+fruits["Pomme"]="vert"
+print(fruits)
+del fruits["banane"]
+print(fruits)
+fruit.keys()
