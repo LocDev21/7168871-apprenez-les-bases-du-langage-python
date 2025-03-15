b738cfd3d2691c4629641a80283269c5e2756308
LocDev21 <layeoumarconde2005@gmail.com>
2025-03-15 00:46:18 +0000

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,7 @@
-# Écrivez votre code ici !
+fruits={"Pomme":"rouge","banane":"jaune","orange":"orange"}
+print(fruits)
+fruits["Kiwi"]="vert"
+print(fruits)
+fruits["Pomme"]="vert"
+fruits.remove["banane"]
+print(fruits)
